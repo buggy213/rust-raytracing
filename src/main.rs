7891c35aa448bcb4a30df602dc95d436c1b334ec
Joshua You <jyou12@berkeley.edu>
2022-06-25 22:36:40 -0700

    normals

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,31 @@ use types::color::Color;
 use types::ray::Ray;
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r) {
-        return Vec3(1.0, 0.0, 0.0);
+    match hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r) {
+        Some(t) => {
+            let normal: Vec3 = Vec3::normalized(r.at(t) - Vec3(0.0, 0.0, -1.0));
+            0.5 * Vec3(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0)
+        },
+        None => {
+            let unit_direction: Vec3 = Vec3::normalized(r.direction);
+            let t = 0.5 * (unit_direction.y() + 1.0);
+            (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+        }
     }
-    let unit_direction: Vec3 = Vec3::normalized(r.direction);
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: Point, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Point, radius: f64, ray: &Ray) -> Option<f64> {
     let oc: Vec3 = ray.origin - center;
     let a = ray.direction.square_magnitude();
     let b = 2.0 * Vec3::dot(oc, ray.direction);
     let c = oc.square_magnitude() - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant > 0.0 {
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
+    }
+    else {
+        None
+    }
 }
 
 fn main() {
@@ -41,11 +51,11 @@ fn main() {
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("255");
 
-    for i in 0..IMAGE_HEIGHT {
-        eprintln!("Scanlines remaining: {}", IMAGE_HEIGHT - i - 1);
-        for j in 0..IMAGE_WIDTH {
-            let u = j as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = i as f64 / (IMAGE_HEIGHT - 1) as f64;
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let ray: Ray = Ray { origin: ORIGIN, direction: LOWER_LEFT + u * HORIZONTAL + v * VERTICAL - ORIGIN };
             let color: Color = ray_color(&ray);
             Color::write_color(color);
